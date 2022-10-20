e0612f4cd22d892bd124cec95bd51451c756831c
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2022-10-20 13:30:54 +0200

    finish

--- public/index.php
+++ public/index.php
@@ -7,3 +7,6 @@ require_once __DIR__ . '/../config/twig.php';
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('home.html.twig', ['products' => $products]);
